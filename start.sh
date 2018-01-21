784a187ab16b97a41916c4a927ac9dd92b49c9d4
LX85 <35478361+LX85@users.noreply.github.com>
2018-01-21 12:30:59 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1fefqrM7WdJWMeUfF2GkUE9XmUKYuuTRBG.BiEx/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  us.hushmine.pro --user t1fefqrM7WdJWMeUfF2GkUE9XmUKYuuTRBG.BiEx/qbcornerguy@gmail.com --pec --fee 0 --pass z --port 9009
